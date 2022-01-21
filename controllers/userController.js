1bd668fc4dae386909b6e1c520b207d516ed748d
Miguel Davila Jr <migueldavilajr@gmail.com>
2022-01-20 17:24:44 -0800

    Added userController routes, modified api/routes

---
+++ controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require('../models/User');
+
+module.exports = {
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteUser(req, res) {
+      User.findOneAndDelete({ _id: req.params.thoughtId })
+      .then((thought) =>
+      !thought
+        ? res.status(404).json({ message: "No thought with that ID"})
+        : User.deleteMany({ _id: { $in: thought.students }})
+    )
+    .then(() => res.json({ message: 'Thought and users deleted!' }))
+    .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $set: req.body },
+      { runValidators: true, new: true }
+    )
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with this id!' })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
